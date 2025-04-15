c24dc8a813911bbbe0bf61f893fe7e52bebef8aa
Burkay <109023686+Rliin@users.noreply.github.com>
2025-04-15 23:55:07 +0300

    Vercel deploy Issue fix

---
+++ page.tsx
@@ -0,0 +1,25 @@
+import Header from "@/app/components/header";
+import Introduction from "@/app/components/introduction";
+import AboutMe from "@/app/components/aboutMe";
+import Skills from "@/app/components/skills";
+import Experience from "@/app/components/experience";
+import Work from "@/app/components/work";
+import Testimonials from "@/app/components/testimonials";
+import GetInTouch from "@/app/components/getInTouch";
+import Footer from "@/app/components/footer";
+
+export default function Home() {
+  return (
+    <div>
+    <Header/>
+    <Introduction/>
+    <AboutMe/>
+    <Skills/>
+    <Experience/>
+    <Work/>
+    <Testimonials/>
+    <GetInTouch/>
+    <Footer/>
+    </div>
+  );
+}
