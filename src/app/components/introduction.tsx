964bafb250a8234694c954d39055f9b6f52907e9
Burkay <109023686+Rliin@users.noreply.github.com>
2025-04-15 23:37:25 +0300

    &apos; Issue fix

--- src/app/components/introduction.tsx
+++ src/app/components/introduction.tsx
@@ -6,8 +6,8 @@ const Introduction = () => {
         <div className="flex justify-around gap-4 p-35 bg-[#030712] text-gray-400">
             <div className="flex flex-col gap-y-15">
                 <div>
-                <h1 className="text-5xl font-bold text-white">Hi, I'm Burkay 👋</h1>
-                <p className="mt-4">I'm a front end developer with a passion for creating user-friendly and efficient web applications.</p>
+                <h1 className="text-5xl font-bold text-white">Hi, I&apos;m Burkay 👋</h1>
+                <p className="mt-4">I&apos;m a front end developer with a passion for creating user-friendly and efficient web applications.</p>
                 </div>
                 <div className="**:flex **:flex-row [&>p]:space-y-4">
                     <p><MapPin/>Eskişehir, Türkiye</p>
