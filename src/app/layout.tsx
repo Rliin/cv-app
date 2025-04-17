1f4d13bc942bfcd6bb484deb652582295eab680e
Burkay <burkay.cetinkaya.ds@gmail.com>
2025-04-17 16:30:27 +0300

    Remove icon.png file and comment out icon entry in layout metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   title: "Burkay",
   description: "Burkay CV-APP",
   icons: {
-    icon: ['/favicon.ico?v=4'],
+    /* icon: ['/favicon.ico?v=4'], */
     apple: ['/apple-touch-icon.png?v=4'],
     shortcut: ['/apple-touch-icon.png'],
   },
