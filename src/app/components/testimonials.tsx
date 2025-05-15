63faf7c5424c0d611dd3c8f4e9d1e2719dd03225
Burkay <burkay.cetinkaya.ds@gmail.com>
2025-05-15 15:23:28 +0300

    Increased Responsiveness for all screen sizes

--- src/app/components/testimonials.tsx
+++ src/app/components/testimonials.tsx
@@ -32,22 +32,25 @@ async function Testimonials() {
           {Array.from({ length: 5 }).map((_, index) => (
             <CarouselItem
               key={index}
-              className="pl-1 md:basis-1/2 lg:basis-1/3"
+              className="pl-1 lg:basis-1/2 xl:basis-1/3"
             >
               <div className="p-2 md:p-2 lg:p-5 hover:scale-101 transition-transform duration-400 ease-in-out">
                 <Card className="bg-white dark:bg-[#1F2937]">
-                  <CardContent className="flex aspect-square justify-center text-center px-5 xs:px-10">
+                  <CardContent className="flex  justify-center px-5 xs:px-10 ">
                       <a
-                        className="flex justify-around flex-col text-xl items-center gap-10 rounded-lg"
+                        className="flex justify-around flex-col items-center gap-10 rounded-lg"
                         href={t(`testimonialLink${index+1}`)}
                         target="_blank"
                       >
-                        <CircleUserRound size={64} color="black" />
-                        <p className="text-left text-base sm:text-lg xl:text-lg text-gray-400 font-light">{t(`testimonialText${index+1}`)}</p>
-
-                        <div className="flex flex-col items-center *:flex *:flex-row *:gap-2 *:items-center *:text-center *:text-pretty">
+                        <CircleUserRound size={64} color="black"/>
+                        <p className="text-left text-base sm:text-lg xl:text-lg text-gray-400 font-light">{t(`testimonialText${index+1}`)}
+                        </p>
+                        <div className="flex flex-col items-center *:flex *:flex-row *:gap-2 *:items-center *:text-center *:text-pretty"
+                        >
                           <h1 className="font-bold text-lg sm:text-xl xl:text-2xl mb-1">{t(`testimonialPerson${index+1}`)}</h1>
-                          <h3 className="text-gray-400 text-base sm:text-xl lg:text-lg">{t(`testimonialTitle${index+1}`)}</h3>
+
+                          <h3 className="text-gray-400 text-base sm:text-xl lg:text-lg">{t(`testimonialTitle${index+1}`)}
+                          </h3>
                         </div>
                       </a>
                   </CardContent>
